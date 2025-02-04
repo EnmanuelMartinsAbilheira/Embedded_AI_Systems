dcb17b5355c96ff6b87c079b89cd8ac272d696ed
Enmanuel Martins <eel2000martins@hotmail.com>
2025-02-04 18:59:44 +0000

    arduino code

--- Exercicio_4/Exercicio_4.ino
+++ Exercicio_4/Exercicio_4.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "cnn.h"
+#include <tflm_esp32.h>
+#include <eloquent_tinyml.h>
+#define ARENA_SIZE 2000 // this is trial-and-error process
+Eloquent::TF::Sequential<TF_NUM_OPS, ARENA_SIZE> tf;
 
+
+void setup() {
+   Serial.begin(115200);
+   delay(3000);
+   Serial.println("__TENSORFLOW IRIS__");
+   // configure input/output
+   tf.setNumInputs(4);
+   tf.setNumOutputs(3);
+   // add required ops
+   tf.resolver.AddFullyConnected();
+   tf.resolver.AddSoftmax();
+   while (!tf.begin(irisModel).isOk()) 
+       Serial.println(tf.exception.toString());
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+   // classify class 0
+   if (!tf.predict(x0).isOk()) {
+       Serial.println(tf.exception.toString());
+       return;
+   }
+   
+   Serial.print("expected class 0, predicted class ");
+   Serial.println(tf.classification);
+   
+   // how long does it take to run a single prediction?
+   Serial.print(tf.benchmark.microseconds());
+   Serial.println("us");
+   
+   delay(1000);
 }
+
